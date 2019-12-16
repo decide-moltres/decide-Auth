ad3e9381335d2320f632aeacb99c1368642b002f
Barranco <rayo329@hotmail.com>
2019-12-16 21:58:48 +0100

    Prueba Issue votación

--- decide/authentication/admin.py
+++ decide/authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import UserProfile
+
+class UserProfileAdmin(admin.ModelAdmin):
+    fields = ['related_political_party', 'birthdate ','sex  ','related_user  ']
+
+admin.site.register(UserProfile, UserProfileAdmin)
 # Register your models here.
