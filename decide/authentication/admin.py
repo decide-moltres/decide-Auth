940eacdba4fc49befa6dc1f0e35ef67fefbf2f16
rayo329 <rayo329@hotmail.com>
2019-12-16 22:44:51 +0100

    Completada la Issue de votación (feature)

--- decide/authentication/admin.py
+++ decide/authentication/admin.py
@@ -2,8 +2,5 @@ from django.contrib import admin
 
 from .models import UserProfile
 
-class UserProfileAdmin(admin.ModelAdmin):
-    fields = ['related_political_party', 'birthdate ','sex  ','related_user  ']
-
-admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserProfile)
 # Register your models here.
--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class UserProfile(AbstractBaseUser):
+class UserProfile(models.Model):
     SEX_OPTIONS = (
         ('M', 'Male'),
         ('F', 'Female'),
         ('O', 'Other'),
     )
-    related_political_party = models.CharField(_('Related political party'),max_length=30, blank=True)
-    birthdate = models.DateField(_('Birthdate'),null=True)
-    sex = models.CharField(_('Sex'),max_length=1, choices=SEX_OPTIONS, null=True)
+    related_political_party = models.CharField(('Related political party'),max_length=30, blank=True)
+    birthdate = models.DateField(('Birthdate'),null=True)
+    sex = models.CharField(('Sex'),max_length=1, choices=SEX_OPTIONS, null=True)
     related_user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
