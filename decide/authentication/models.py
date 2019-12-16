f0055a2e093e5ace4792fd9d409d3cc5413a6240
Barranco <rayo329@hotmail.com>
2019-12-16 21:40:27 +0100

    Solving Issue "Solicitud de votación..." (feature)

--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class UserProfile(AbstractBaseUser):
+    SEX_OPTIONS = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+    related_political_party = models.CharField(_('Related political party'),max_length=30, blank=True)
+    birthdate = models.DateField(_('Birthdate'),null=True)
+    sex = models.CharField(_('Sex'),max_length=1, choices=SEX_OPTIONS, null=True)
+    related_user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
