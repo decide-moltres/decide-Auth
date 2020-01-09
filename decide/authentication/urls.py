96733139c2970bf23266bd20d30816538526178a
GuillermoDP14 <guigarjim@alum.us.es>
2020-01-09 19:27:11 +0100

    Mejoras en la presentación de las páginas
    Se han cambiando el color de fondo de la cabecera y el menu, el color del texto y su orientación en este han cambiado y los colores de fondo de los botones de iniciar sesión mediante redes sociales se han cambiado según la red social.
    
    Referencia: #74

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -17,14 +17,14 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
 
     path('', views.home, name='home'),	
-	  url(r'^login/$', login, {'template_name': 'registrarion/login.html'}, name='login'),
+	url(r'^login/$', login, {'template_name': 'registrarion/login.html'}, name='login'),
     path('signup/', views.register, name='signup'),		   
     path('accounts/', include('django.contrib.auth.urls')),	
     url(r'^oauth/', include('social_django.urls', namespace="social")),
-	  url(r'^profile/$', views.view_profile, name="view_profile"),
-	  url(r'^profile/edit/$', views.edit_profile, name="edit_profile"),
-	  url(r'^profile/password/$', views.change_password, name='change_password'),
-    path('policy', views.policy), 
+	url(r'^profile/$', views.view_profile, name="view_profile"),
+	url(r'^profile/edit/$', views.edit_profile, name="edit_profile"),
+	url(r'^profile/password/$', views.change_password, name='change_password'),
+    path('policy/', views.policy, name='policy'), 
 	
 
 
