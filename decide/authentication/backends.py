799d1fe25f7e16cade112e2c4d6071aa2495e72e
Guillermo Garrido Jimenez <56034163+GuillermoDP14@users.noreply.github.com>
2020-01-12 09:58:17 +0100

    Cambios en los comentarios del fichero backends.py

--- decide/authentication/backends.py
+++ decide/authentication/backends.py
@@ -2,15 +2,13 @@ from django.contrib.auth import backends
 from django.contrib.auth.models import User
 
 class EmailAuthBackend(backends.ModelBackend):
-    """
-    Email Authentication Backend
-
-    Allows a user to sign in using an email/password pair, then check
-    a username/password pair if email failed
+    
+    """ Backend que usa el par correo electrónico y contraseña para iniciar sesión, si la primera comprobación
+        falla, entonces lo comprueba mediante el par nombre de usuario y contraseña.
     """
 
     def authenticate(self, username=None, password=None):
-        """ Authenticate a user based on email address as the user name. """
+        """ Autentificación del usuario usando su correo electrócnico como si fuese su nombre de usuario """
         try:
             user = User.objects.get(email=username)
             if user.check_password(password):
@@ -24,7 +22,7 @@ class EmailAuthBackend(backends.ModelBackend):
                 return None
 
     def get_user(self, user_id):
-        """ Get a User object from the user_id. """
+        """ Mediante la id se consigue al User"""
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
